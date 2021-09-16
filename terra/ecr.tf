d95de74336d351bbd9dcdcc5000c9a280bf40ddf
utkarsh <utkarsh@utkarshs-MacBook-Air.local>
2021-09-16 08:06:26 +0530

    ecr creation

---
+++ terra/ecr.tf
@@ -0,0 +1,30 @@
+resource "aws_ecr_repository" "test-repository" {
+  name                 = "test-repo"
+  image_tag_mutability = "IMMUTABLE"
+}
+
+resource "aws_ecr_repository_policy" "test-repo-policy" {
+  repository = aws_ecr_repository.test-repository.name
+  policy     = <<EOF
+  {
+    "Version": "2008-10-17",
+    "Statement": [
+      {
+        "Sid": "adds full ecr access to the this repository",
+        "Effect": "Allow",
+        "Principal": "*",
+        "Action": [
+          "ecr:BatchCheckLayerAvailability",
+          "ecr:BatchGetImage",
+          "ecr:CompleteLayerUpload",
+          "ecr:GetDownloadUrlForLayer",
+          "ecr:GetLifecyclePolicy",
+          "ecr:InitiateLayerUpload",
+          "ecr:PutImage",
+          "ecr:UploadLayerPart"
+        ]
+      }
+    ]
+  }
+  EOF
+}
